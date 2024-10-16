999693ee64e7eb457d52501139072240963e4bfb
Kartikn-code <ping2karti@gmail.com>
2024-10-16 16:35:11 +0530

    2nd commit

--- login/urls.py
+++ login/urls.py
@@ -1,10 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import login,signin
+from .views import login,signin,userentry
 
 urlpatterns=[
     path('userlogin/',login,name="login"),
     path('usersignin/',signin,name="signin"),
+    path('user_entry/',userentry,name="userentry"),
 ]
 
--- login/views.py
+++ login/views.py
@@ -12,10 +12,12 @@ def authentication(request):
     if  request.method=="POST":
         username=request.POST.get('Username')
         password=request.POST.get('password')
-        if username=="karthi@1234" & password=="H@ppy2004":
+        if username=="ping2karti2gmail.com" & password=="H@ppy2004":
             return redirect('mainpage')
 
 def mainpage(request):
     return render(request,"mainpage.html")
 def signin(request):
     return render(request,"signin.html")
+def userentry(request):
+    return render(request,"entry.html")
