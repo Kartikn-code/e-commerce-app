2a63c6f88ccbb540a62417bb4dd2e4d3b45ae691
Kartikn-code <ping2karti@gmail.com>
2024-10-17 17:09:29 +0530

    js added

---
+++ login/static/java-script/script.js
@@ -0,0 +1,8 @@
+  var loginbox= document.querySelector(".loginbox")
+var appear =document.querySelector(".appear")
+
+
+appear.addEventListener("click",function(){
+    loginbox.style.display="block"
+})
+
